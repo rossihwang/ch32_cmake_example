9c4f29cc17c8e12cdfa93d90455c539e85ed00a3
rossihwang <rossihwang@gmail.com>
2023-07-22 13:42:42 +0800

    Add paths for MRS built-in openocd

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+# MUST set according to your environment
 export MRS_PATH=PATH_TO_MRS_Community
 export CH32_EXAM_PATH=PATH_TO_EXAM
 
+export OPENOCD_PATH=${MRS_PATH}/toolchain/OpenOCD/bin
 export TOOLCHAIN_PREFIX=${MRS_PATH}/toolchain/"RISC-V Embedded GCC"
-
+export PATH=${OPENOCD_PATH}:${PATH}
 
