d78fbfdfe6db5f3e6df2210e36818421b67aa89c
rossihwang <rossihwang@gmail.com>
2023-07-23 10:15:45 +0800

    Fix and improve compilation

--- riscv.cmake
+++ riscv.cmake
@@ -13,8 +13,32 @@ message(VERBOSE "CROSS_COMPILER: " ${CROSS_COMPILER})
 
 set(TOOLCHAIN_BIN_DIR ${TOOLCHAIN_PREFIX}/bin)
 
+set(OPTIONS
+  # Target Processor
+  -march=rv32imac
+  -mabi=ilp32
+  -msmall-data-limit=8
+  -mno-save-restore
+  # Optimization
+  -Os
+  -fmessage-length=0
+  -fsigned-char
+  -ffunction-sections
+  -fdata-sections
+  -fno-common
+  # Warning
+  -Wunused
+  -Wuninitialized
+)
 # Compilers settings
-set(CMAKE_C_COMPILE_FEATURES -specs=nosys.specs)
+add_compile_options(
+  ${OPTIONS}
+)
+
+add_link_options(
+  ${OPTIONS}
+)
+
 set(CMAKE_C_COMPILER ${TOOLCHAIN_BIN_DIR}/${CROSS_COMPILER}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_BIN_DIR}/${CROSS_COMPILER}g++)
 set(CMAKE_ASM_COMPILER ${TOOLCHAIN_BIN_DIR}/${CROSS_COMPILER}gcc)
@@ -26,6 +50,7 @@ set(CMAKE_CXX_COMPILER_WORKS ON)
 # Other tools settings
 set(CMAKE_OBJCOPY ${TOOLCHAIN_BIN_DIR}/${CROSS_COMPILER}objcopy)
 set(CMAKE_OBJDUMP ${TOOLCHAIN_BIN_DIR}/${CROSS_COMPILER}objdump)
+set(CMAKE_SIZE ${TOOLCHAIN_BIN_DIR}/${CROSS_COMPILER}size)
 
 # Libc settings
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
